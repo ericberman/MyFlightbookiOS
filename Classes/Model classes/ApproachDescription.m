c277db28da7c9a1a7e696281effa62c7cc3474e4
Eric Berman <ericberman@users.noreply.github.com>
2019-08-10 13:39:42 -0700

    Fix issue #166

--- Classes/Model classes/ApproachDescription.m
+++ Classes/Model classes/ApproachDescription.m
@@ -1,7 +1,7 @@
 /*
 	MyFlightbook for iOS - provides native access to MyFlightbook
 	pilot's logbook
- Copyright (C) 2017 MyFlightbook, LLC
+ Copyright (C) 2017-2019 MyFlightbook, LLC
  
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -78,7 +78,7 @@
 }
 
 + (NSArray<NSString *> *) ApproachSuffixes {
-    return @[@"", @"-A", @"-B", @"-C", @"-X", @"-Y", @"-Z"];
+    return @[@"", @"-A", @"-B", @"-C", @"-D", @"-X", @"-Y", @"-Z"];
 }
 
 + (NSArray<NSString *> *) RunwayNames {
