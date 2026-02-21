b56edfdf224f4c9f2e2fb4e05a935d5609a0900a
Eric Berman <ericberman@users.noreply.github.com>
2026-02-21 08:17:32 -0800

    Fix typo

--- Classes/Model classes/Category Extensions/MFBWebService LogbookEntry Extensions.swift
+++ Classes/Model classes/Category Extensions/MFBWebService LogbookEntry Extensions.swift
@@ -644,7 +644,7 @@ extension MFBWebServiceSvc_LogbookEntry : AutoDetectDelegate {
         addProperty(NSNumber(integerLiteral: PropTypeID.nameOfSIC.rawValue), withString:dict["flight_selectedCrewSIC"] as? String)
         addProperty(NSNumber(integerLiteral: PropTypeID.nameOfCFI.rawValue), withString:dict["flight_selectedCrewInstructor"] as? String)
         addProperty(NSNumber(integerLiteral: PropTypeID.nameOfStudent.rawValue), withString:dict["flight_selectedCrewStudent"] as? String)
-        addProperty(NSNumber(integerLiteral: PropTypeID.flightNum.rawValue), withString: dict["flight_flightNumber"] as? String])
+        addProperty(NSNumber(integerLiteral: PropTypeID.flightNum.rawValue), withString: dict["flight_flightNumber"] as? String)
         
         aircraftID = NSNumber(integerLiteral: -1)
         if let ac = Aircraft.sharedAircraft.AircraftByTail((dict["flight_selectedAircraftID"] as? String) ?? "") {
